e26cf30b7f20bbe00ac397b86de77ee3dc0348b5
Mohammed Aasif <mohammedaasif@tecnovators.in>
2021-02-22 16:40:17 +0530

    add message controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :loged_in?
+  helper_method :current_user, :logged_in?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def require_user
-    if !loged_in?
+    if !logged_in?
       flash[:error] ="You must be logged in to perform that action"
       redirect_to login_path
     end
